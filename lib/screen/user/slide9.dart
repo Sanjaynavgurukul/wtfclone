11b11f993ae2f347562a619dfc6f350ffcc33f03
= <sanjaycizmar@gmail.com>
2022-03-07 18:00:56 +0530

    choose body type

--- lib/screen/user/slide9.dart
+++ lib/screen/user/slide9.dart
@@ -1,3 +1,4 @@
+import 'package:expansion_tile_card/expansion_tile_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
@@ -49,6 +50,7 @@ class _Slide9State extends State<Slide9> {
           'assets/svg/${locator<AppPrefs>().gender.getValue().toLowerCase()}/veryFat.svg'
     },
   ];
+
   @override
   void initState() {
     final user = Provider.of<UserController>(context, listen: false);
@@ -61,51 +63,117 @@ class _Slide9State extends State<Slide9> {
   @override
   Widget build(BuildContext context) {
     return Consumer<UserController>(
-      builder: (context, user, child) => Column(
-        children: [
-          Text(
-            "Select your body type",
-            style: TextStyle(
-              fontSize: 22.0,
-              color: Colors.white,
-            ),
-          ),
-          UIHelper.verticalSpace(20.0),
-          Container(
-            height: Get.height * 0.6,
-            width: Get.width,
-            child: GridView.builder(
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: types.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-              ),
-              itemBuilder: (context, index) => _item(
-                title: types[index]['type'],
-                onPress: () {
-                  setState(() {
-                    bodyType = types[index]['type'];
-                    print('Body Type: ${types[index]}');
-                    user.setValue(bodyType: types[index]['type']);
-                  });
-                },
-                isSelected: bodyType == types[index]['type'],
-                image: types[index]['image'],
-              ),
-            ),
-          ),
-          UIHelper.verticalSpace(8.0),
-          Text(
-            AppConstants.confidentialInfo,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 12.0,
-              color: AppColors.TEXT_DARK,
-            ),
-          ),
-        ],
+      builder: (context, user, child) => SingleChildScrollView(
+        child: Column(
+          children: [
+            ExpansionTileCard(
+              elevation: 0,
+              baseColor: Color(0xff922224),
+              expandedColor: Color(0xff922224),
+              title: Text('Choose your body type',
+                  style: TextStyle(color: Colors.white)),
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: Color(0xff292929),
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(8),
+                          bottomRight: Radius.circular(8))),
+                  padding: EdgeInsets.all(12),
+                  width: double.infinity,
+                  child: Wrap(
+                      alignment: WrapAlignment.start,
+                      runAlignment: WrapAlignment.start,
+                      crossAxisAlignment: WrapCrossAlignment.start,
+                      runSpacing: 0.0,
+                      spacing: 12.0,
+                      children: types
+                          .map((e) => newUI(
+                              data: e,
+                              selected: bodyType == e['type'],
+                              onClick: () {
+                                // print('clicked value');
+                                setState(() {
+                                  bodyType = e['type'];
+                                  user.setValue(bodyType: e['type']);
+                                });
+                              }))
+                          .toList()),
+                )
+              ],
+            )
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget newUI(
+      {Function onClick, Map<String, String> data, bool selected = false}) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 12),
+      child: InkWell(
+        onTap: onClick,
+        child: Container(
+          child: Text(
+            data['type'] ?? 'No Type',
+            style: TextStyle(color: selected ? Colors.black : Colors.white),
+          ),
+          padding: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(4)),
+              color: selected ? Colors.white : Colors.transparent,
+              border: Border.all(width: 1, color: Colors.white)),
+        ),
+      ),
+    );
+  }
+
+  Widget oldUI(UserController user) {
+    return Column(
+      children: [
+        Text(
+          "Select your body type",
+          style: TextStyle(
+            fontSize: 22.0,
+            color: Colors.white,
+          ),
+        ),
+        UIHelper.verticalSpace(20.0),
+        Container(
+          height: Get.height * 0.6,
+          width: Get.width,
+          child: GridView.builder(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: types.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+            itemBuilder: (context, index) => _item(
+              title: types[index]['type'],
+              onPress: () {
+                setState(() {
+                  bodyType = types[index]['type'];
+                  print('Body Type: ${types[index]}');
+                  user.setValue(bodyType: types[index]['type']);
+                });
+              },
+              isSelected: bodyType == types[index]['type'],
+              image: types[index]['image'],
+            ),
+          ),
+        ),
+        UIHelper.verticalSpace(8.0),
+        Text(
+          AppConstants.confidentialInfo,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 12.0,
+            color: AppColors.TEXT_DARK,
+          ),
+        ),
+      ],
     );
   }
 
