c71ed9cbea417b3f592ac02a90d376d8935b76af
= <sanjaycizmar@gmail.com>
2022-05-17 01:47:18 +0530

    xcode error resolved

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,8 +1,6 @@
 import UIKit
 import Flutter
 import GoogleMaps
-import Firebase
-import FirebaseMessaging
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -13,32 +11,8 @@ import FirebaseMessaging
 //    if #available(iOS 10.0, *) {
 //      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
 //    }
-    if #available(iOS 10.0, *) {
-          UNUserNotificationCenter.current().delegate = self
-          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-          UNUserNotificationCenter.current().requestAuthorization(
-            options: authOptions,
-            completionHandler: {_, _ in })
-        } else {
-          let settings: UIUserNotificationSettings =
-          UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-          application.registerUserNotificationSettings(settings)
-        }
-    application.registerForRemoteNotifications()
-        FirebaseApp.configure()
     GMSServices.provideAPIKey("AIzaSyCyEYyFZfeeLHpDuyfXCBjpDZf8IfqPYk0")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-    
-//     https://firebase.google.com/docs/auth/ios/phone-auth#appendix:-using-phone-sign-in-without-swizzling
-    // https://firebase.google.com/docs/cloud-messaging/ios/client#token-swizzle-disabled
-    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        // Pass device token to auth
-        
-        // Pass device token to messaging
-        Messaging.messaging().apnsToken = deviceToken
-        
-        return super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-    }
 }
