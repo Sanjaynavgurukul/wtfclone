e1cc0338647fd2935b770842b4cc606743e7545e
= <sanjaycizmar@gmail.com>
2022-05-26 16:17:45 +0530

    new schedule model added

---
+++ lib/model/new_schedule_model.dart
@@ -0,0 +1,146 @@
+class NewScheduleModel {
+  //Default Constructor D:
+  NewScheduleModel({this.status, this.newScheduleCat, this.newScheduleData});
+
+  bool status;
+  List<NewScheduleData> newScheduleData;
+  List<NewScheduleCat> newScheduleCat;
+
+  factory NewScheduleModel.fromJson(Map<String, dynamic> json) =>
+      NewScheduleModel(
+        status: json["status"],
+        newScheduleData: json.containsKey('data')
+            ? List<NewScheduleData>.from(
+                json["data"].map((x) => NewScheduleData.fromJson(x)))
+            : [],
+        newScheduleCat: json.containsKey('categories')
+            ? List<NewScheduleCat>.from(
+                json["categories"].map((x) => NewScheduleCat.fromJson(x)))
+            : [],
+      );
+}
+
+class NewScheduleCat {
+  NewScheduleCat({this.name});
+
+  String name;
+
+  factory NewScheduleCat.fromJson(Map<String, dynamic> json) => NewScheduleCat(
+        name: json["name"],
+      );
+}
+
+class NewScheduleData {
+  int set_no;
+  List<NewScheduleDataExercises> exercises;
+
+  NewScheduleData({this.exercises, this.set_no});
+
+  factory NewScheduleData.fromJson(Map<String, dynamic> json) =>
+      NewScheduleData(
+        // uid: json["uid"],
+        set_no: json["set_no"],
+        exercises: json.containsKey('exercises')
+            ? List<NewScheduleData>.from(json["exercises"]
+                .map((x) => NewScheduleDataExercises.fromJson(x)))
+            : [],
+      );
+}
+
+class NewScheduleDataExercises {
+  NewScheduleDataExercises(
+      {this.user_id,
+      this.uid,
+      this.added_by,
+      this.addon_id,
+      this.category_id,
+      this.category_image,
+      this.category_name,
+      this.date,
+      this.date_added,
+      this.day,
+      this.description,
+      this.e_calories,
+      this.e_duration,
+      this.ex_no,
+      this.ex_seq,
+      this.gym_id,
+      this.is_pt,
+      this.package_id,
+      this.reps,
+      this.sets,
+      this.status,
+      this.t_duration,
+      this.video,
+      this.wo_name,
+      this.workout_id});
+
+  String uid;
+  String category_name; //null": "Chest",
+  String category_id; //null": "SCk4hG6DISBDB",
+  String workout_id; //null": "OIiiQLfaPVILU",
+  String user_id; //null": "4q60Mk7GJhyx9",
+  String description; //null"
+  String status; //null": "expire",
+  String date_added; //null": "1653296278859",
+  String date; //null": "23-05-2022",
+  String reps; //null": "20,15,10",
+  String e_duration; //null": "",
+  String e_calories; //null": "0",
+  String sets; //null": "4",
+  int is_pt; //null": 0,
+  String addon_id; //null": "",
+  String added_by; //null": "WaJ7DhoCJDu4F",
+  String gym_id; //null": "GLKdIYAWDS2Q8",
+  String t_duration; //null": "",
+  String package_id; //null": "GJvY8ilW7TPEO",
+  int ex_seq; //null": 1,
+  String day; //null": "day1",
+  int ex_no; //null": 1,
+  String wo_name; //null": "Incline Bench Press",
+  String video; //null":
+  String category_image; //null":
+
+  static String getStringData(String data) {
+    if (data == null || data.isEmpty)
+      return '';
+    else
+      return data;
+  }
+
+  static int getIntData(String data) {
+    if (data == null || data.isEmpty)
+      return 0;
+    else
+      return int.parse(data);
+  }
+
+  factory NewScheduleDataExercises.fromJson(Map<String, dynamic> json) =>
+      NewScheduleDataExercises(
+        uid: json["uid"],
+        category_name: getStringData(json["category_name"]),
+        category_id: getStringData(json["category_id"]),
+        workout_id: getStringData(json["workout_id"]),
+        user_id: getStringData(json["user_id"]),
+        description: getStringData(json["description"]),
+        status: getStringData(json["status"]),
+        date_added: getStringData(json["date_added"]),
+        date: getStringData(json["date"]),
+        reps: getStringData(json["reps"]),
+        e_duration: getStringData(json["e_duration"]),
+        e_calories: getStringData(json["e_calories"]),
+        sets: getStringData(json["sets"]),
+        is_pt: getIntData(json["is_pt"].toString()),
+        addon_id: getStringData(json["addon_id"]),
+        added_by: getStringData(json["added_by"]),
+        gym_id: getStringData(json["gym_id"]),
+        t_duration: getStringData(json["t_duration"]),
+        package_id: getStringData(json["package_id"]),
+        ex_seq: getIntData(json["ex_seq"].toString()),
+        day: getStringData(json["day"]),
+        ex_no: getIntData(json["ex_no"].toString()),
+        wo_name: getStringData(json["wo_name"]),
+        video: getStringData(json["video"]),
+        category_image: getStringData(json["category_image"]),
+      );
+}
--- lib/screen/schedule/new_ui/exercises_screen.dart
+++ lib/screen/schedule/new_ui/exercises_screen.dart
@@ -1,8 +1,11 @@
 import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/material.dart';
+import 'package:wtf/helper/app_constants.dart';
+import 'package:wtf/helper/colors.dart';
 
 class ExercisesScreen extends StatefulWidget {
   static const String routeName = '/exercisesScreen';
+
   const ExercisesScreen({Key key}) : super(key: key);
 
   @override
@@ -10,57 +13,325 @@ class ExercisesScreen extends StatefulWidget {
 }
 
 class _ExercisesScreenState extends State<ExercisesScreen> {
+  double recommendedCircleHeight = 80.00;
+
+  static const double EMPTY_SPACE = 10.0;
+  ScrollController _scrollController;
+  bool isScrolledToTop = true;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
+  _scrollListener() {
+    if (_scrollController.offset <= _scrollController.position.minScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      //call setState only when values are about to change
+      if(!isScrolledToTop) {
+        setState(() {
+          //reach the top
+          isScrolledToTop = true;
+        });
+      }
+
+    }else{
+      //call setState only when values are about to change
+      if(_scrollController.offset > EMPTY_SPACE && isScrolledToTop) {
+        setState(() {
+          //not the top
+          isScrolledToTop = false;
+        });
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        automaticallyImplyLeading: false,
-        leading: IconButton(
-          onPressed: ()=>Navigator.pop(context),
-          icon: Icon(Icons.arrow_back_ios),
-        ),
-        title: Text('Exercise'),
-      ),
-      body: Container(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                height: 300,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Colors.grey,
-                          Colors.transparent,
-                        ])
-                ),
-              ),
-              DatePicker(
-                DateTime.now(),
-                initialSelectedDate: DateTime.now(),
-                selectionColor: Colors.white,
-                selectedTextColor: Colors.black,
-                deactivatedColor: Colors.white,
-
-                onDateChange: (date) {
-                  // New date selected
-                  setState(() {
-                  });
-                },
-              ),
-            ],
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          elevation: isScrolledToTop ? 0 : 4,
+          backgroundColor: isScrolledToTop?Colors.transparent:AppColors.BACK_GROUND_BG,
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: Icon(Icons.arrow_back_ios),
           ),
+          title: Text('Exercise'),
         ),
-      )
+        body: Container(
+          child: SingleChildScrollView(
+            controller: _scrollController,
+            padding: EdgeInsets.only(bottom: 80),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: 300,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                colors: [
+                                  Colors.grey,
+                                  Colors.transparent,
+                                ])),
+                      ),
+                      Positioned(
+                        right: 0,
+                        child: SafeArea(
+                          child: Container(
+                            constraints: BoxConstraints(minWidth: 100),
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius:BorderRadius.only(topLeft: Radius.circular(100),bottomLeft: Radius.circular(100))
+                            ),
+                            child: Text('19:00:00',style:TextStyle(fontSize: 20)),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                DatePicker(
+                  DateTime.now(),
+                  initialSelectedDate: DateTime.now(),
+                  selectionColor: Colors.white,
+                  selectedTextColor: Colors.black,
+                  onDateChange: (date) {
+                    // New date selected
+                    setState(() {});
+                  },
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                Container(
+                  padding: EdgeInsets.all(18),
+                  margin: EdgeInsets.only(left: 16, right: 16),
+                  alignment: Alignment.center,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(100)),
+                      color: AppConstants.bgColor),
+                  child: Text(
+                    'Start Workout',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                SizedBox(
+                  height: 26,
+                ),
+                Padding(
+                    padding: EdgeInsets.only(left: 16, right: 16),
+                    child: Text(
+                      'Today\'s Schedule',
+                      style: TextStyle(fontSize: 18),
+                    )),
+                SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 16),
+                  child: SingleChildScrollView(
+                    padding: EdgeInsets.only(right: 16),
+                    child: Row(
+                      children: [
+                        scheduleExerciseItem(),
+                        scheduleExerciseItem(),
+                        scheduleExerciseItem(),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: getList().length,
+                      itemBuilder: (context,index){
+                        ExModel item  = getList()[index];
+
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                            child: Text('Set ${index+1} (${item.label})',style: TextStyle(fontWeight:FontWeight.bold,fontSize: 18),),padding: EdgeInsets.only(left: 16,right: 16),),
+                        SizedBox(height: 12,),
+                        itemCard(selected: index == 1,item: item)
+                      ],
+                    );
+                  }),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Widget scheduleExerciseItem() {
+    return Container(
+      margin: EdgeInsets.only(right: 12),
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            width: recommendedCircleHeight,
+            height: recommendedCircleHeight,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(100)),
+                gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [Color(0xffE00303), Color(0xff485AFE)])),
+            child: Container(
+              margin: EdgeInsets.all(2),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(100)),color: Colors.grey.shade200)),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: -10,
+
+            child: Container(
+              padding: EdgeInsets.only(top: 2,bottom: 2),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(100)),
+                  gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [Color(0xffE00303), Color(0xff485AFE)]),border: Border.all(width: 3,color: AppColors.BACK_GROUND_BG)
+              ),
+              child: Text('Chest'),
+            ),
+          )
+        ],
+      ),
     );
   }
+
+  Widget itemCard({bool selected = false,ExModel item}){
+    return Column(
+      children: item.data.map((e) => Container(
+        margin: EdgeInsets.only(left: 16,right: 16,bottom: 18),
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+            border: Border.all(width: selected?1:0,color:!selected?Colors.transparent: Colors.grey),
+            borderRadius: BorderRadius.all(Radius.circular(8))
+        ),child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(
+                    Radius.circular(8)
+                ),
+                color: Colors.green,
+              ),
+              height: 80,
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              margin: EdgeInsets.only(left: 12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 40,
+                    height: 8,
+                    decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.all(Radius.circular(100))
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.all(Radius.circular(100))
+                    ),
+                    child: Text('4/4'),
+                    padding: EdgeInsets.only(left: 10,right: 10,top: 2,bottom: 2),
+                  ),
+                  ListTile(
+                    contentPadding: EdgeInsets.only(left: 0,right: 0,top: 0,bottom: 0),
+                    title: Text('Skull Candy'),
+                    trailing: Container(
+                      padding: EdgeInsets.only(left: 8,right: 8,top: 2,bottom: 2),
+                      decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.all(Radius.circular(100))
+                      ),
+                      child: Text("Resume"),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text('Set : 4'),
+                        Text('Reps : 12,14,15,16')
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+      )).toList(),
+    );
+  }
+
+  List<ExModel> getList()=>[
+    ExModel(label: 'Chest',data: [
+      'Something','Something','Something',
+    ]),
+    ExModel(label: 'Cardio',data: [
+      'Something',
+    ]),
+    ExModel(label: 'Biceps',data: [
+      'Something','Something','Something','Something',
+    ]),
+    ExModel(label: 'Kabutar Dance',data: [
+      'Something','Something','Something',
+    ]),
+  ];
+
 }
+
+class ExModel{
+  String label;
+  List<String> data;
+  ExModel({this.data,this.label});
+}
+
+
